17991ad4fcca3749df985ea31df2f8307497b6bd
Harieo <harieo@hotmail.com>
2022-12-06 11:35:47 +0000

    Added a save method to SchematicStorage

--- paper/src/main/java/net/harieo/schematics/paper/config/SchematicStorage.java
+++ paper/src/main/java/net/harieo/schematics/paper/config/SchematicStorage.java
@@ -1,59 +1,74 @@
 package net.harieo.schematics.paper.config;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.common.collect.ImmutableSet;
+import com.google.gson.*;
+import com.google.gson.stream.JsonWriter;
 import net.harieo.schematics.modification.Modification;
 import net.harieo.schematics.paper.modification.registry.BukkitJsonBlueprintRegistry;
 import net.harieo.schematics.schematic.Schematic;
 import net.harieo.schematics.serialization.Blueprint;
 import net.harieo.schematics.serialization.Deserializer;
+import net.harieo.schematics.serialization.impl.schematic.SchematicJsonBlueprint;
 import net.harieo.schematics.serialization.impl.schematic.SchematicJsonDeserializer;
+import net.harieo.schematics.serialization.impl.schematic.SchematicJsonSerializer;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Unmodifiable;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FilenameFilter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.FileSystemException;
 import java.nio.file.NotDirectoryException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.Function;
 
 /**
  * A handler for reading and writing schematic files in storage.
  */
 public class SchematicStorage {
 
+    // Static constants
     public static final String DEFAULT_SUBDIRECTORY_NAME = "schematics";
 
-    private final SchematicJsonDeserializer schematicJsonDeserializer;
+    // Fields for file management
+    private final SchematicJsonBlueprint schematicJsonBlueprint;
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create(); // For file writing in pretty format
+
+    // Cache
     private final Set<Schematic> schematics = new HashSet<>();
 
     /**
-     * Constructs this storage instance with a {@link SchematicJsonDeserializer} provided to allow deserialization from
-     * storage.
+     * Constructs this storage instance with a {@link SchematicJsonBlueprint} to permit serialization and deserialization
+     * for file writing and reading.
      *
-     * @param schematicJsonDeserializer the schematic deserializer which will be able to deserialize schematic files
+     * @param schematicJsonBlueprint for serializing and deserializing schematics
      */
-    public SchematicStorage(@NotNull SchematicJsonDeserializer schematicJsonDeserializer) {
-        this.schematicJsonDeserializer = schematicJsonDeserializer;
+    public SchematicStorage(@NotNull SchematicJsonBlueprint schematicJsonBlueprint) {
+        this.schematicJsonBlueprint = schematicJsonBlueprint;
     }
 
     /**
+     * <p>
      * Constructs this storage instance with a {@link SchematicJsonDeserializer} formed from all the deserializers
-     * in {@link BukkitJsonBlueprintRegistry} for deserialization modifications from storage.
-     *
+     * in {@link BukkitJsonBlueprintRegistry} for deserializing modifications from storage.
+     * </p>
+     * <p>
+     * A default {@link SchematicJsonSerializer} is then created to form the {@link SchematicJsonBlueprint}
+     * required for this class.
+     *</p>
      * @param bukkitJsonBlueprintRegistry the blueprint registry for modifications
      */
     @SuppressWarnings("unchecked")
     public SchematicStorage(@NotNull BukkitJsonBlueprintRegistry bukkitJsonBlueprintRegistry) {
-        this(new SchematicJsonDeserializer());
+        SchematicJsonDeserializer schematicJsonDeserializer = new SchematicJsonDeserializer();
         bukkitJsonBlueprintRegistry.getBlueprints().stream()
                 .map(Blueprint::getDeserializer)
                 .map(deserializer -> (Deserializer<Modification, JsonObject>) deserializer)
                 .forEach(schematicJsonDeserializer::addModificationDeserializer);
+        // A serializer is created by default in the blueprint, so we do not need to create one for this constructor
+        this.schematicJsonBlueprint = new SchematicJsonBlueprint(schematicJsonDeserializer);
     }
 
     /**
@@ -93,7 +108,7 @@ public class SchematicStorage {
                 JsonElement schematicRawElement = JsonParser.parseReader(reader);
                 if (schematicRawElement.isJsonObject()) {
                     JsonObject serializedSchematic = schematicRawElement.getAsJsonObject();
-                    schematics.add(schematicJsonDeserializer.deserialize(serializedSchematic));
+                    schematics.add(schematicJsonBlueprint.deserialize(serializedSchematic));
                 } else {
                     plugin.getLogger().warning("Unable to deserialize a schematic JSON object in "
                             + schematicFile.getName());
@@ -108,7 +123,7 @@ public class SchematicStorage {
      * Loads schematics from the schematic subdirectory within the plugin folder where the subdirectory name is
      * the {@link #DEFAULT_SUBDIRECTORY_NAME}.
      *
-     * @param plugin the plugin managing the schematic files
+     * @param plugin         the plugin managing the schematic files
      * @param filenameFilter an optional {@link FilenameFilter} for scanning files
      * @throws IOException see description from super method {@link #load(Plugin, String, FilenameFilter)}
      */
@@ -125,6 +140,77 @@ public class SchematicStorage {
      */
     public void load(@NotNull Plugin plugin) throws IOException {
         load(plugin, DEFAULT_SUBDIRECTORY_NAME, null);
+    }
+
+    /**
+     * @return an immutable set of all cached schematics
+     */
+    public @Unmodifiable Set<Schematic> getSchematics() {
+        return ImmutableSet.copyOf(schematics);
+    }
+
+    /**
+     * Adds a schematic to the cache, but does not save it to storage.
+     *
+     * @param schematic the schematic to cache
+     */
+    public void addSchematic(@NotNull Schematic schematic) {
+        schematics.add(schematic);
+    }
+
+    /**
+     * Removes a schematic from the cache, but does not edit the file storage.
+     *
+     * @param schematic the schematic to remove from the cache
+     */
+    public void removeSchematic(@NotNull Schematic schematic) {
+        schematics.remove(schematic);
+    }
+
+    /**
+     * Saves all schematics cached in this instance.
+     *
+     * @param plugin the plugin managing the schematic files
+     * @param subDirectoryName the subdirectory of the plugin directory where schematics files should be saved
+     * @param fileNameGenerationFunction a function to generate the name of the file for each schematic
+     * @param overwrite whether to overwrite existing files with the same name of a schematic
+     * @throws IOException if the subdirectory does not exist and cannot be created
+     * @throws NotDirectoryException if the subdirectory specified is not a directory
+     * @throws FileAlreadyExistsException if overwriting is permitted but a file cannot be overwritten due to an error
+     * @return true if all schematics were successfully saved, or false if one or more schematics could not be saved.
+     * @apiNote If this method returns false, it will be because overwriting was not permitted and a file with the name
+     * already exists.
+     */
+    public boolean save(@NotNull Plugin plugin,
+                     @NotNull String subDirectoryName,
+                     @NotNull Function<Schematic, String> fileNameGenerationFunction,
+                     boolean overwrite) throws IOException {
+        File schematicDirectory = new File(plugin.getDataFolder(), subDirectoryName);
+        if (!schematicDirectory.exists() && !schematicDirectory.mkdir()) {
+            throw new IOException("Failed to create schematics directory");
+        } else if (!schematicDirectory.isDirectory()) {
+            throw new NotDirectoryException(schematicDirectory.getAbsolutePath() + " is not a directory");
+        }
+
+        boolean allSchematicsSaved = true;
+        for (Schematic schematic : schematics) { // Using for loop to include exceptions in 'throws' clause
+            String fileName = fileNameGenerationFunction.apply(schematic);
+            File schematicFile = new File(schematicDirectory, fileName);
+            if (schematicFile.exists()) {
+                if (overwrite && !schematicFile.delete()) { // Attempt to delete file for overwrite, exception on failure
+                    throw new FileAlreadyExistsException("Unable to overwrite schematic file " + schematicFile.getAbsolutePath());
+                } else {
+                    allSchematicsSaved = false;
+                    continue; // No authority to overwrite file, so this schematic will be ignored
+                }
+            }
+
+            try (FileWriter writer = new FileWriter(schematicFile)) {
+                writer.write(gson.toJson(schematicJsonBlueprint.serialize(schematic)));
+            }
+        }
+
+        return allSchematicsSaved;
     }
 
 }
