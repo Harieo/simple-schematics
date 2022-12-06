821006c1f96d10e1c637f9017c6187caad772c0a
Harieo <harieo@hotmail.com>
2022-12-06 11:41:30 +0000

    Added default SchematicStorage to SchematicsPlugin

--- paper/src/main/java/net/harieo/schematics/paper/SchematicsPlugin.java
+++ paper/src/main/java/net/harieo/schematics/paper/SchematicsPlugin.java
@@ -3,20 +3,29 @@ package net.harieo.schematics.paper;
 import co.aikar.commands.PaperCommandManager;
 import net.harieo.schematics.paper.command.CommandPosition;
 import net.harieo.schematics.paper.command.SchematicCommand;
+import net.harieo.schematics.paper.config.SchematicStorage;
 import net.harieo.schematics.paper.config.SchematicToolConfiguration;
+import net.harieo.schematics.paper.modification.registry.BukkitJsonBlueprintRegistry;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
+/**
+ * The {@link JavaPlugin} which allows this library to be loaded as a standalone plugin.
+ *
+ * @apiNote To access this internal class, please use {@link JavaPlugin#getPlugin(Class)}.
+ */
 public class SchematicsPlugin extends JavaPlugin {
 
     private SchematicToolConfiguration schematicToolConfiguration;
+    private SchematicStorage schematicStorage;
 
     @Override
     public void onEnable() {
         this.schematicToolConfiguration = new SchematicToolConfiguration();
+        this.schematicStorage = new SchematicStorage(new BukkitJsonBlueprintRegistry());
 
         try {
             schematicToolConfiguration.load(this);
@@ -36,6 +45,10 @@ public class SchematicsPlugin extends JavaPlugin {
 
     public SchematicToolConfiguration getSchematicToolConfiguration() {
         return schematicToolConfiguration;
+    }
+
+    public SchematicStorage getSchematicStorage() {
+        return schematicStorage;
     }
 
 }
