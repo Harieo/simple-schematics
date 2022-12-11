7e395527cb59cccd476c4f9ca5cb5693583bfe67
Harieo <harieo@hotmail.com>
2022-12-11 19:12:46 +0000

    Added SerializableEmptyTransition and AnimationCommand

---
+++ common/src/main/java/net/harieo/schematics/animation/impl/serializable/SerializableEmptyTransition.java
@@ -0,0 +1,48 @@
+package net.harieo.schematics.animation.impl.serializable;
+
+import com.google.gson.JsonObject;
+import net.harieo.schematics.animation.Transition;
+import net.harieo.schematics.animation.serialization.TransitionJsonSerializable;
+import net.harieo.schematics.serialization.Serializer;
+import net.harieo.schematics.serialization.impl.transition.TransitionJsonSerializer;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A {@link Transition} which has no action when run to add a delay to the animation sequence.
+ */
+public class SerializableEmptyTransition extends Transition implements TransitionJsonSerializable {
+
+    // There are no configurable or overridable values, so this can just be constructed
+    private final EmptyTransitionJsonSerializer emptyTransitionJsonSerializer = new EmptyTransitionJsonSerializer();
+
+    /**
+     * A transition with an amount of milliseconds before it should run and an amount of milliseconds before a proceeding
+     * transition should run.
+     *
+     * @param millisecondsBefore the milliseconds before this transition should run
+     * @param millisecondsAfter  the milliseconds before the next transition should run proceeding this one
+     */
+    public SerializableEmptyTransition(long millisecondsBefore, long millisecondsAfter) {
+        super(millisecondsBefore, millisecondsAfter);
+    }
+
+    @Override
+    public void run() {
+        // No action taken
+    }
+
+    @Override
+    public @NotNull Serializer<Transition, JsonObject> getSerializer() {
+        return emptyTransitionJsonSerializer;
+    }
+
+    public static class EmptyTransitionJsonSerializer extends TransitionJsonSerializer {
+
+        @Override
+        public final void addExtraData(@NotNull Transition transition, @NotNull JsonObject serializedObject) {
+            // No extra data exists for this transition type
+        }
+
+    }
+
+}
---
+++ paper/src/main/java/net/harieo/schematics/paper/command/animation/AnimationCommand.java
@@ -0,0 +1,8 @@
+package net.harieo.schematics.paper.command.animation;
+
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.Subcommand;
+
+@Subcommand("animation|simpleanimation|animations|simpleanimations")
+public class AnimationCommand extends BaseCommand {
+}
